4d17970625f920673ee8c23cef58e99c162d8a21
davidindra22 <daviedindra20@gmail.com>
2026-03-05 15:11:16 +0700

    add code port

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Express API!" });
 });
 
+const PORT = process.env.PORT || 5000;
+
 // admin
 app.get("/api/admin", async (req, res) => {
   try {
@@ -922,4 +924,4 @@ app.patch(
   },
 );
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
